c4cdca8f0ebfa8ce91b8359e909d0d2ac3f3cf80
eshasherry <esha.sherry@gmail.com>
2023-03-10 18:45:09 -0500

    adding a Formik Form to TodoComponent.jsx

--- src/components/TodoComponent.jsx
+++ src/components/TodoComponent.jsx
@@ -1,12 +1,16 @@
 import {useParams} from "react-router-dom";
 import {useAuth} from "../security/AuthContext";
 import {getTodo} from "../api/TodoServiceApi";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
+import {Field, Form, Formik} from "formik";
 
 export default function TodoComponent(){
     const {id} = useParams()
     const authContext = useAuth()
     const username = authContext.username
+    const [description, setDescription] = useState('')
+    const [targetDate, setTargetDate] = useState('')
+
 //refresh only when the id value changes
     useEffect(
         ()=>todo(),
@@ -14,12 +18,33 @@ export default function TodoComponent(){
     )
     function todo(){
         getTodo(username, id)
-            .then((response)=>console.log(response))
+            .then((response)=> {
+                setDescription(response.data.description)
+                setTargetDate(response.data.targetDate)
+            })
             .catch(error=>console.log(error))
     }
     return(
         <div>
             <h3>Update your Task</h3>
+            <div>
+                <Formik initialValues={{description, targetDate}} enableReinitialize={true}>
+                    {
+                        (props)=>(
+                            <Form>
+                                <fieldset className="form-group">
+                                    <label>Description</label>
+                                    <Field type="text" className="form-control" name="description"/>
+                                </fieldset>
+                                <fieldset className="form-group">
+                                    <label>Target Date</label>
+                                    <Field type="date" className="form-control" name="targetDate"/>
+                                </fieldset>
+                            </Form>
+                        )
+                    }
+                </Formik>
+            </div>
         </div>
     )
 }
